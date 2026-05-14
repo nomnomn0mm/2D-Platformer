fdc0c3e806885fdbb5866c52a946630889b3da60
nomnomnomm <fikwong@s.sfusd.edu>
2026-05-14 14:12:38 -0700

    player animations done

--- Scripts/player.gd
+++ Scripts/player.gd
@@ -38,7 +38,7 @@ func _physics_process(delta):
 
 func _process(delta):
 	if velocity.x != 0:
-		sprite.flip_h = velocity.x > 0
+		sprite.flip_h = velocity.x < 0
 		
 	if global_position.y > 200:
 		game_over()
